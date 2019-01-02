10bf1669fcf81d9c21755f0c7a558e408090f260
ongzhixian <zhixian@hotmail.com>
2019-01-02 17:01:27 +0800

    toto

--- toto.py
+++ toto.py
@@ -43,6 +43,25 @@ appconfig = app_helpers.appconfig
 #from pages import *
 
 ################################################################################
+# Functions
+################################################################################
+
+def initialize_sqlite_db(sqlite_filename):
+    """Initialize a sqlite3 database for tracking ASX sector indices.
+
+    Table(s) created:
+
+    1.  sector_indices
+
+    Args:
+        N/A
+
+    Returns:
+        N/A
+    """
+    logging.info("Ensuring existence of sqlite3 database [{0}]".format(sqlite_filename))
+
+################################################################################
 # Main function
 ################################################################################
 
@@ -55,7 +74,47 @@ if __name__ == '__main__':
     logging.debug("%8s test message %s" % ("DEBUG", str(datetime.utcnow())))
     # Initial modules that requires it
 
+    #from os import path
+    from modules import toto_data
+    toto_data.init()
+    #toto_data.load_csv('./data/csv/toto-2018.csv')
+    toto_data.csv_to_db('./data/csv/toto-2018.csv')
     
+    
+
+    # import os
+    
+    # data_directoryPath = './data/csv'
+    # sqlite_fileName = 'toto-2018.sqlite3'
+    # sqlite_filePath = os.path.join(data_directoryPath, sqlite_fileName)
+    #sqlite_filename = './data/csv/toto-2018.sqlite3'
+
+    # Read a csv file and dump contents into a Sqlite3 database.
+    # import sqlite3
+    # import csv
+
+    # with open('./data/csv/toto-2018.csv', 'rb') as csvfile:
+    #     csv_reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+    #     # csv_reader.next() # Use next() to rows such as headers.
+    #     for row in csv_reader:
+    #         print(row)
+
+    # with sqlite3.connect(sqlite_filePath) as conn:
+    #     cursor = conn.cursor()
+    #     # ZX: Rememeber there are only 5 data types in Sqlite3: text, numeric, integer, real, blob
+    #     cursor.execute('''CREATE TABLE IF NOT EXISTS raw (
+    #         draw_number         numeric ,
+    #         draw_date           text,
+    #         num1                numeric,
+    #         num2                numeric,
+    #         num3                numeric,
+    #         num4                numeric,
+    #         num5                numeric,
+    #         num6                numeric,
+    #         num7                numeric,
+    #         remarks             text,
+    #         PRIMARY KEY (draw_number)
+    #         )''')
     
     
     logging.info("[PROGRAM END]")
