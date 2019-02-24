d19f02de0cb0ee8db24e37be67d0c30611448dc0
Ong Zhixian <overxianz@gmail.com>
2019-02-24 18:56:58 +0800

    add how to mindmap

---
+++ data/mindmaps/how-to.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1550965250576" ID="ID_1423161913" MODIFIED="1550965356515" TEXT="how-to">
+<node CREATED="1550965334723" ID="ID_29278510" MODIFIED="1550965349432" POSITION="right" TEXT="python">
+<node CREATED="1550965358582" ID="ID_1522771086" MODIFIED="1550965367928" TEXT="requests">
+<node CREATED="1550965369582" ID="ID_1098260058" MODIFIED="1550965384907" TEXT="suppress warning for SSL"/>
+<node CREATED="1550965385533" ID="ID_253956200" MODIFIED="1550965392931" TEXT="indicate CA_BUNDLE"/>
+</node>
+</node>
+</node>
+</map>
