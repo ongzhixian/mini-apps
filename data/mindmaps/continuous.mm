0a89ce9bc1e5ce4cbdb47548b1c5be292963b208
Ong Zhixian <overxianz@gmail.com>
2019-03-24 08:33:49 +0800

    update

---
+++ data/mindmaps/continuous.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1553386793071" ID="ID_1091199692" MODIFIED="1553386802121" TEXT="continuous">
+<node CREATED="1553386804645" ID="ID_783880467" MODIFIED="1553386809321" POSITION="right" TEXT="delivery"/>
+<node CREATED="1553386810292" ID="ID_693198099" MODIFIED="1553386814393" POSITION="left" TEXT="integration"/>
+</node>
+</map>
